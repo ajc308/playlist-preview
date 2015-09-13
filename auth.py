59abdc7210c0309f5281be3e35387991d50c80d6
ajc308 <andrew.connor@beatport.com>
2015-09-13 12:59:51 -0400

    Remove auth

--- auth.py
+++ auth.py
@@ -1,3 +1,3 @@
 import os
 def get_apix_auth_headers():
-    return {'Authorization': 'Token {}'.format(os.environ.get('APIX_AUTH_TOKEN') or 'd1185029eeb8fd261e630e3d796ca71928ad33b4')}
+    return {'Authorization': 'Token {}'.format(os.environ.get('APIX_AUTH_TOKEN'))}
